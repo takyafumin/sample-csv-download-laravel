7db4f5d695051e2c0a66d060bd241c0b954bb43b
Takafumi Fukai <takyafumin@gmail.com>
2024-11-21 20:57:43 +0900

    refactor, コードの整形と不要なコメントを削除

--- app/Http/Controllers/User/UserDownloadController.php
+++ app/Http/Controllers/User/UserDownloadController.php
@@ -12,31 +12,29 @@ class UserDownloadController extends Controller
 {
     /**
      * CSVヘッダー
+     *
      * @var string[]
      */
     private const CSV_HEADER = ['id', 'name', 'email', 'created_at', 'updated_at'];
 
     /**
      * CSVファイル名
+     *
      * @var string
      */
     private const CSV_FILENAME = 'users.csv';
 
-    /**
-     * @param UserDownloadUsecase $usecase
-     */
     public function __construct(
         private UserDownloadUsecase $usecase
     ) {}
 
     /**
      * ユーザーデータをCSVでダウンロード
-     * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
      */
     public function __invoke(): \Symfony\Component\HttpFoundation\BinaryFileResponse
     {
         // ユーザーデータ取得
-        $users   = $this->usecase->exec();
+        $users = $this->usecase->exec();
         $csvData = $users->toArray();
 
         // ダウンロード用一時ファイル作成
@@ -46,20 +44,19 @@ class UserDownloadController extends Controller
         return response()
             ->download($tempFile, self::CSV_FILENAME, [
                 'Content-Type' => 'text/csv',
-                'Content-Disposition' => 'attachment; filename="' . self::CSV_FILENAME . '"',
+                'Content-Disposition' => 'attachment; filename="'.self::CSV_FILENAME.'"',
             ])
             ->deleteFileAfterSend(true);
     }
 
     /**
-     * @param array $csvData
      * @return 一時ファイル名
      */
     private function createDownloadFile(array $csvData): string
     {
         // タイムスタンプを使用して一時ファイルの名前を決定
         $timestamp = now()->format('YmdHis');
-        $tempFile = sys_get_temp_dir() . "/users_{$timestamp}.csv";
+        $tempFile = sys_get_temp_dir()."/users_{$timestamp}.csv";
 
         // 一時ファイルを開く
         $handle = fopen($tempFile, 'w');
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
 use App\Models\Project;
 use App\Models\Task;
 use App\Models\User;
+use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
