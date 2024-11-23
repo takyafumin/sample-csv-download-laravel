658adf5db1b1f512a1c9a771c479ac71e3d60331
Takafumi Fukai <takyafumin@gmail.com>
2024-11-23 15:33:15 +0900

    update, ProjectsExportWithChunk.php でカラム数によるメモリ使用量調査結果を追加

--- packages/Project/Exports/ProjectsExportWithChunk.php
+++ packages/Project/Exports/ProjectsExportWithChunk.php
@@ -54,13 +54,29 @@ class ProjectsExportWithChunk implements FromQuery, WithCustomChunkSize, WithHea
 
         // これはメモリエラーになる
         // 127MB
+        // return [
+        //     $row->id,
+        //     $row->name,
+        //     $row->description,
+        //     $row->created_at,
+        //     $row->updated_at,
+        // ] + explode(',', $row->tasks);
+
+        // 10: 60MB
+        // 11: 64MB
+        // 12: 67MB
+        // 13: 71MB
+        // 14: 88MB
+        // 15: 90MB
+        // 19: 105MB
+        // 20: 109MB
         return [
             $row->id,
             $row->name,
             $row->description,
             $row->created_at,
             $row->updated_at,
-        ] + explode(',', $row->tasks);
+        ] + range(1, 19);
     }
 
     public function chunkSize(): int
